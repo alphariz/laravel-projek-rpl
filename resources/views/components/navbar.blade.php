20931b636a2be962ae69d7c1ae1b54e0a20376d5
Alfa <rizymuhammad@gmail.com>
2022-04-16 20:36:34 +0700

    Add Navbar as Component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,27 @@
+<nav>
+    <div class="container px-6 py-3 mx-auto lg:flex lg:justify-between lg:items-center">
+        <div class="flex items-center justify-between">
+            <div>
+                <a class="text-2xl font-bold text-gray-800 dark:text-white lg:text-3xl hover:text-gray-700 dark:hover:text-gray-300" href="#">Brand</a>
+            </div>
+
+            <!-- Mobile menu button -->
+            <div class="flex lg:hidden">
+                <button type="button" class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400" aria-label="toggle menu">
+                    <svg viewBox="0 0 24 24" class="w-6 h-6 fill-current">
+                        <path fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
+                    </svg>
+                </button>
+            </div>
+        </div>
+
+        <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
+        <div class="items-center lg:flex">
+            <div class="flex flex-col lg:flex-row lg:mx-6">
+                @foreach ($navigation as $name => $url)
+                <a class="my-1 text-sm font-medium text-gray-700 dark:text-gray-200 hover:text-blue-500 dark:hover:text-blue-400 lg:mx-4 lg:my-0" href={{ $url }}>{{ $name }}</a>
+                @endforeach
+            </div>
+        </div>
+    </div>
+</nav>
