a1e40a151d38a522ecdcdce44c41374f23fe4bfe
Alfa <rizymuhammad@gmail.com>
2022-04-16 20:25:23 +0700

    Add AppLayout as Master Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-    <h1 class="text-8xl text-fuchsia-500 font-bold">Hello World</h1>
-</body>
-</html>
+<x-app-layout>
+
+</x-app-layout>
