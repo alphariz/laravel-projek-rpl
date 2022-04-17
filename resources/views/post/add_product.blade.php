598a1bffcdf651c88c490e221a1a5b089ceb8fcd
Alfa <rizymuhammad@gmail.com>
2022-04-17 12:22:24 +0700

    Add Add Product and Edit Product Page

--- resources/views/post/add_product.blade.php
+++ resources/views/post/add_product.blade.php
@@ -25,6 +25,7 @@
                 <textarea class="block w-full h-48 px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"></textarea>
                 {{-- <textarea type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required> --}}
             </div>
+
         </form>
     </div>
 </x-app-layout>
