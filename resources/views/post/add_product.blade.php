5845a4590c1b58a7fc7d7545f282a8c7dd795f98
Alfa <rizymuhammad@gmail.com>
2022-04-17 13:28:08 +0700

    Delete create and edit method from Product Controller

--- resources/views/post/add_product.blade.php
+++ resources/views/post/add_product.blade.php
@@ -25,7 +25,7 @@
                 <textarea class="block w-full h-48 px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"></textarea>
                 {{-- <textarea type="text" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required> --}}
             </div>
-
+            <button type="submit" class="px-3 py-1 text-sm font-bold text-gray-100 transition-colors duration-200 transform bg-gray-600 rounded cursor-pointer hover:bg-gray-500">Add New Product</button>
         </form>
     </div>
 </x-app-layout>
