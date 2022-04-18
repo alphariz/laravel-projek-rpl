347c7f6ebd747343e1544fe27a6694e8ecc0609c
Alfa <rizymuhammad@gmail.com>
2022-04-18 21:03:11 +0700

    Modify view on edit product page

--- resources/views/post/edit_product.blade.php
+++ resources/views/post/edit_product.blade.php
@@ -6,7 +6,7 @@
             @method('put')
             @csrf
             <div class="flex divide-x-2">
-                <div class="w-1/2 mr-4">
+                <div class="w-1/2">
                     <div class="mb-6">
                         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Product Name</label>
                         <input type="text" id="name" name="name" value="{{ $product->name }}" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
@@ -21,7 +21,7 @@
                     </div>
                 </div>
                 <div class="w-1/2 ml-4">
-                    <div class="w-full p-4">
+                    <div class="w-[21rem] m-3 p-2 border-2 rounded-lg shadow-md">
                         <img class="object-cover w-full h-60 mx-auto block" id="image" name="image" src="{{ asset('storage/'.$product->image_path) }}" alt="">
                     </div>
                 </div>
