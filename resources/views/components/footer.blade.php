ae84d7d0a395bd435257763356adc9697188f593
Alfa <rizymuhammad@gmail.com>
2022-06-01 21:23:20 +0700

    Remove edit&delete from landingpage product list

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,7 +3,7 @@
         <hr class="mt-2 mb-4 border-gray-200 dark:border-gray-700" />
 
         <div class="flex flex-col items-center sm:flex-row sm:justify-between">
-            <p class="text-sm text-gray-400">© Copyright 2022. Hak Cipta.</p>
+            <p class="text-sm text-gray-400">Copyright &copy; 2022. Hak Cipta.</p>
 
             <div class="flex mt-3 -mx-2 sm:mt-0">
                 <span class="mx-2 text-sm text-gray-400 hover:text-gray-500 dark:hover:text-gray-300">Firsta Rahmania Sucahyo</span>
